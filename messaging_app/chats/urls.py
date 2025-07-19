df44d061de9f5674b26f08defc7c32fe9e9ff1fd
Isaiah Kimoban <67864381+kimoban@users.noreply.github.com>
2025-07-19 22:53:59 +0000

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -17,6 +17,11 @@ router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
+# Create nested router for messages within conversations
+conversations_router = NestedDefaultRouter(
+    router, r"conversations", lookup="conversation"
+)
+
 urlpatterns = [
     # Include the router-generated URLs
     path('', include(router.urls)),
