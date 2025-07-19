141d1be88b6d690d22320c598c376fe20c4c2b3a
Isaiah Kimoban <67864381+kimoban@users.noreply.github.com>
2025-07-19 22:59:20 +0000

    Update urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("chats.urls")),
+    path('api-auth/', include('rest_framework.urls')),  # DRF auth for browseable API
 ]
