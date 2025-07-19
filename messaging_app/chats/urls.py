767042cfc9ed706ba89b0cf274edb0ecae384738
Isaiah Kimoban <67864381+kimoban@users.noreply.github.com>
2025-07-19 22:29:39 +0000

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,9 +3,10 @@ from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
 router = DefaultRouter()
-router.register(r'conversations', ConversationViewSet)
-router.register(r'messages', MessageViewSet)
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
+    routers.DefaultRouters()
     path('', include(router.urls)),
 ]
