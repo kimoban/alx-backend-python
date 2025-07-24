ee52744d17118a5f290fac31af5fe9bb33a92e7d
Isaiah Kimoban <67864381+kimoban@users.noreply.github.com>
2025-07-24 19:29:43 +0000

    Update settings.py

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -153,7 +153,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+"chats.middleware.RolepermissionMiddleware",
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
