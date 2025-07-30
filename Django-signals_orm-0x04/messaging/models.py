9b6213d7fdf8cb7ca8ac12a46da6ad11fee8d9f7
Isaiah Kimoban <67864381+kimoban@users.noreply.github.com>
2025-07-30 03:04:16 +0000

    Update models.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+edited_by
 
 class UnreadMessagesManager(models.Manager):
     """Custom manager for filtering unread messages for a specific user."""
